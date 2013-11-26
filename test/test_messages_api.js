ed5367aab6a570e3a5e6e857c34871d80ea9f276
Jamie Bate <jamie@tidepool.org>
2013-11-26 20:44:09 +1300

    cleanup and add port as param in tests

--- test/test_messages_api.js
+++ test/test_messages_api.js
@@ -2,9 +2,11 @@
 
 var should = require('chai').should(),
     supertest = require('supertest'),
-    api = supertest('http://localhost:8081');
+    config = require('../env');
 
 describe('/message', function() {
+
+    var api = supertest('http://localhost:'+config.port); 
 
   it('returns message for given id as JSON', function(done) {
     api.get('/api/v1/message/121')
